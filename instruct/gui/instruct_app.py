dba5707210b1d6b83318ee3440b201f8bf4e8152
xbasset <xavier.basset@hoomano.com>
2024-09-29 23:15:14 +0200

    wip: simulate streaming working

--- instruct/gui/instruct_app.py
+++ instruct/gui/instruct_app.py
@@ -14,11 +14,38 @@ from textual.reactive import reactive
 
 from textual import work
 
+final_content = """
+Here is a sonnet about Allemont and its surroundings:
 
-class InstructLayout(App):
+In Allemont, where nature's beauty shines,
+Le Cornillon's peaks invite you to ascend,
+Belledonne's grandeur beckons, a divine
+Experience awaiting, for hikers to amend.
+
+Le Taillefer's slopes, a cyclist's delight,
+Rolling hills and valleys, a scenic ride,
+While l'Étendard's forests whisper secrets bright,
+A haven for nature lovers, side by side.
+
+In summer, wildflowers bloom, a colorful sight,
+In winter, snowflakes dance, a winter wonderland,
+Allemont, a haven, where outdoor dreams take flight,
+A destination for adventure, at your command.
+
+So come, dear traveler, and let your spirit soar,
+In Allemont, where nature's beauty leaves you more.
+
+I hope you enjoy it!"""
+
+
+# Method to simulate a text streaming that outputs the final_content word by word with a delay of 0.05 seconds per word, and takes a callback function as an argument that is called after each word is simulately generated.
+import time
+
+
+class InstructApp(App):
     SAVED_FILE_EXT = ".md"
 
-    CSS_PATH = "layout.tcss"
+    CSS_PATH = "instruct_app.tcss"
 
     BINDINGS = [
         Binding(key="q", action="quit", description="Quit the app"),
@@ -62,16 +89,26 @@ class InstructLayout(App):
         self.temperature = temperature
         self.forced_model = forced_model  # TODO implement this in instruct call
 
+        self.first_token_reiceved = False
+        self.instruct = Instruct(
+            filepath=self.instruct_file, **self.input if self.input else {}
+        )
+        self.model = (
+            self.instruct.matching_model.name
+        )  # TODO: fix the update model name in the top menu
+        
+
     def compose(self) -> ComposeResult:
         yield Header(show_clock=False)
         yield TopMenu(
-            [None, str(self.temperature), str(self.max_tokens)], id="top_menu"
-        ).data_bind(InstructLayout.model)
+            [str(self.model), str(self.temperature), str(self.max_tokens)],
+            id="top_menu",
+        ).data_bind(InstructApp.model)
 
-        yield ResultViewer(id="result_viewer").data_bind(InstructLayout.content)
+        yield ResultViewer(id="result_viewer").data_bind(InstructApp.content)
         yield Footer()
 
-    def on_mount(self):
+    async def on_mount(self):
         self.title = "Instruct"
         self.sub_title = "Result Viewer"
         result_viewer = self.query_one("#result_viewer")
@@ -79,25 +116,44 @@ class InstructLayout(App):
         result_viewer.styles.border = ("heavy", "white")
         top_menu = self.query_one("#top_menu")
         top_menu.styles.height = "1fr"
-        result_viewer.loading = True
-        self.call_after_refresh(self.run_instruct)
 
-    @work
+        await self.run_instruct()
+        # self.call_after_refresh(self.run_instruct)
+        # self.content = final_content
+
+    @work(thread=True)
+    def simulate_text_streaming(self, callback):
+        words = final_content.split()
+        complete_text = ""
+        for word in words:
+            complete_text += word + " "
+            self.app.call_from_thread(callback, complete_text)
+            time.sleep(0.05)
+        return final_content
+
+
+    # @work(thread=True)
     async def run_instruct(self):
         try:
-            instruct = Instruct(
-                filepath=self.instruct_file, **self.input if self.input else {}
-            )
-            self.model = instruct.matching_model.name # TODO: fix the update model name in the top menu
+
             # self.notify(f"Running with model: {self.model}")
 
-            result = instruct.run(
-                temperature=self.temperature, max_tokens=self.max_tokens
-            )
-            self.content = result
-            self.query_one("#result_viewer").loading = False
+            # result = self.instruct.run(
+            #     temperature=self.temperature, max_tokens=self.max_tokens,
+            #     stream=True,
+            #     stream_callback=self._token_received
+            # )
+            result = self.simulate_text_streaming(self._token_received)
+            # self.query_one("#result_viewer").loading = False
         except Exception as e:
             self.notify(f"Error running instruct: {e}", severity="error", title="Error")
+
+    def _token_received(self, token):
+        try:
+            self.content = token
+            self.refresh(layout=True)
+        except Exception as e:
+            self.notify(f"Error updating content: {e}", severity="error", title="Error")
 
     def action_help(self):
         self.notify("This is the GUI of `instruct`", severity="information")
@@ -135,7 +191,7 @@ class InstructLayout(App):
         directory, base_name = os.path.split(file_path)
         base, _ = os.path.splitext(base_name)
 
-        saved_file_ext = InstructLayout.SAVED_FILE_EXT
+        saved_file_ext = InstructApp.SAVED_FILE_EXT
         file_path = os.path.join(directory, base + saved_file_ext)
 
         if not os.path.exists(file_path):
@@ -153,6 +209,6 @@ class InstructLayout(App):
         )
 
 
-# if __name__ == "__main__":
-#     app = InstructLayout()
-#     app.run()
+if __name__ == "__main__":
+    app = InstructApp()
+    app.run()
