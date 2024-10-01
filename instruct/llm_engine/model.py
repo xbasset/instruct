625e19e417c19ee3ed66afae078a0c9d2fa0b9ad
xbasset <xavier.basset@hoomano.com>
2024-10-01 15:56:33 +0200

    Refactor llm_engine with litellm

--- instruct/llm_engine/model.py
+++ instruct/llm_engine/model.py
@@ -1,52 +1,39 @@
-from abc import ABC, abstractmethod
-import json
 import logging
-import os
+from litellm import completion
 
 logging.basicConfig(level=logging.ERROR)
 
 
-class Model(ABC):
+class Model:
     """
     Abstract base class for Model implementations.
     """
 
-    @abstractmethod
-    def __init__(self, model_conf):
+    def __init__(
+        self,
+        model: str,
+        name: str,
+        api_key: str = None,
+        api_version: str = None,
+        base_url: str = None,
+        **kwargs,
+    ):
         """
         Initializes the model object.
 
         Args:
             model_conf (dict): The configuration for the model.
         """
-        pass
+        self.model = model
+        self.name = name
+        self.api_key = api_key
+        self.api_version = api_version
+        self.base_url = base_url
 
-    @property
-    def name(self):
-        return self._name
 
-    @name.setter
-    def name(self, value):
-        self._name = value
-
-    @property
-    def type(self):
-        return self._type
-
-    @type.setter
-    def type(self, value):
-        self._type = value
-
-    @property
-    def provider(self):
-        return self._provider
-
-    @provider.setter
-    def provider(self, value):
-        self._provider = value
-
-    @abstractmethod
-    def invoke(self, messages, temperature, max_tokens):
+    def invoke(
+        self, messages, temperature, max_tokens, stream=False, stream_callback=None
+    ):
         """
         Perform a Model chat completion.
 
@@ -54,31 +41,65 @@ class Model(ABC):
             messages (list): List of messages.
             temperature (float): Temperature parameter for generating responses.
             max_tokens (int): Maximum number of tokens in the generated response.
+            stream (bool): Whether to stream the response.
+            stream_callback (function): Callback function for streaming.
 
         Returns:
             None
         """
-        pass
+        try:
+            completion_result = completion(
+                model=self.model,
+                api_key=self.api_key,
+                api_version=self.api_version,
+                base_url=self.base_url,
+                messages=messages,
+                stream=stream,
+                temperature=temperature,
+                max_tokens=max_tokens,
+            )
 
-    @abstractmethod
-    def invoke_from_pt(self, instruct, temperature, max_tokens):
+            if stream:
+                complete_text = ""
+                for stream_chunk in completion_result:
+                    complete_text += stream_chunk.choices[0].delta.content or ""
+                    if stream_callback is not None:
+                        try:
+                            stream_callback(complete_text)
+                        except Exception as e:
+                            logging.error(f"🔴 Error in stream_callback: {e}")
+
+                response = complete_text
+            else:
+                response = completion_result["choices"][0]["message"]["content"]
+            return response
+        except Exception as e:
+            logging.error(f"Error in Model invoke: {e}")
+
+    def interpret(
+        self, instruct, temperature, max_tokens, stream=False, stream_callback=None
+    ):
         """
-        Perform a Model chat completion using the provided Instruct.
+        Perform the interpretation of an Instruct object using the Model.
 
         Args:
             instruct (Instruct): The Instruct object.
             temperature (float): Temperature parameter for generating responses.
             max_tokens (int): Maximum number of tokens in the generated response.
+            stream (bool): Whether to stream the response.
+            stream_callback (function): Callback function for streaming.
 
         Returns:
             None
         """
-        pass
-
-    @abstractmethod
-    def chatCompletion(self, *args, **kwargs):
-        """
-        Abstract method that should be implemented to handle chat completions. The parameters for each subclass that
-        implements this method may vary, hence the use of *args and **kwargs to accept any number of arguments.
-        """
-        pass
+        try:
+            messages = [{"role": "user", "content": instruct.prompt}]
+            return self.invoke(
+                messages=messages,
+                temperature=temperature,
+                max_tokens=max_tokens,
+                stream=stream,
+                stream_callback=stream_callback,
+            )
+        except Exception as e:
+            logging.error(f"Error in Model interpret: {e}")
